6742430ff26675d1fea38009ff3442b9b37562e3
EROS <yassine.kader2017fc@gmail.com>
2023-03-06 00:10:52 +0100

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+var admin = express();
+var PORT = 3000;
+  
+admin.on('mount', function (parent) {
+  console.log('Admin Mounted');
+});
+  
+admin.get('/', function (req, res) {
+  res.send('Admin Homepage');
+});
+  
+app.use('/admin', admin);
+  
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
+
+/*
+const port = 3000
+app.listen(port, ()=>{
+    console.log("server running on port "+port)
+})*/
