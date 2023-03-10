0f1e3c5e48c73b1671c12f93824b2097abbaa2c0
EROS <yassine.kader2017fc@gmail.com>
2023-03-10 22:52:04 +0100

    update

--- client/url_shortner/src/components/LoginForm.jsx
+++ client/url_shortner/src/components/LoginForm.jsx
@@ -1,7 +1,14 @@
-import "./css/LoginForm.css"
- export default function LoginForm(){
-    return(
-        <>
-        </>
-    )
- }
+import "./css/LoginForm.css";
+export default function LoginForm() {
+  return (
+    <div className="container">
+      <form>
+          <label>Username:</label>
+          <input type={"text"}></input>
+          <label>Password:</label>
+          <input type={"password"}></input>
+        <a href="something.com">Forgot your password ?</a>
+      </form>
+    </div>
+  );
+}
